83e4a1184734a4e5e2b47c778eb117b5fa08fcd5
unknown <alijhlan01@gmail.com>
2022-12-09 08:00:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 11, 0, 167),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Ali.jpg'),
+              ),
+              Text(
+                ' علي بن جحلان',
+                style: TextStyle(
+                  fontFamily: 'Cairo',
+                  fontSize: 38.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'مبرمج تطبيقات',
+                style: TextStyle(
+                  color: Colors.grey.shade200,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.cyan[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.cyan[700],
+                  ),
+                  title: Text(
+                    '715924217',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.cyan[700],
+                  ),
+                  title: Text(
+                    'alijhlan01@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.hub,
+                    color: Colors.cyan[700],
+                  ),
+                  title: Text(
+                    'https://github.com/ALI-tech-tech',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
